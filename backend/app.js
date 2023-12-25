52c0608e1e5b4989ab338dcc0b312a4fe0c88bfe
seunghwan <sun1238799@naver.com>
2023-12-25 21:26:18 +0900

    fix: 도커파일 수정

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.set("port", 8081);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 
 app.listen(app.get("port"), () => {
